54bf3fc81b4d4d28f0b624c9cf3aef39c3e864f0
Luís Felipe Santos <luisfelipejesussantos123@gmail.com>
2025-06-06 14:55:28 -0300

    Update super_trunfo_parte2.c

--- super_trunfo_parte2.c
+++ super_trunfo_parte2.c
@@ -144,7 +144,7 @@
                         printf ("População de %s: %lu habitantes\n", pais_1, populacao_1);
                         printf ("População de %s: %lu habitantes\n", pais_2, populacao_2);
                     }
-                     else if (populacao_1 = populacao_2) {
+                     else if (populacao_1 == populacao_2) {
                          printf ("EMPATE\n");
                          printf ("Atributo: População");
                          printf ("População de %s: %lu habitantes\n", pais_1, populacao_1);
@@ -168,7 +168,7 @@
                         printf ("Área (em km²) de %s: %.2f km²\n", pais_1, area_1);
                         printf ("Área (em km²) de %s: %.2f km²\n", pais_2, area_2);
                     }
-                    else if (area_1 = area_2) {
+                    else if (area_1 == area_2) {
                         printf ("EMPATE\n");
                         printf ("Atributo: Área");
                         printf ("Área (em km²) de %s: %.2f km²\n", pais_1, area_1);
@@ -192,7 +192,7 @@
                         printf ("PIB de %s: R$%.2f\n", pais_1, pib_1);
                         printf ("PIB de %s: R$%.2f\n", pais_2, pib_2);
                     }
-                    else if (pib_1 = pib_2) {
+                    else if (pib_1 == pib_2) {
                          printf ("EMPATE\n");
                          printf ("Atributo: PIB (Produto Interno Bruto)");
                          printf ("PIB de %s: R$%.2f\n", pais_1, pib_1);
@@ -216,7 +216,7 @@
                         printf ("Número de Pontos Turísticos de %s: %d\n", pais_1, num_pontos_turisticos_1);
                         printf ("Número de Pontos Turísticos de %s: %d\n", pais_2, num_pontos_turisticos_2);
                     }
-                    else if (num_pontos_turisticos_1 = num_pontos_turisticos_2) {
+                    else if (num_pontos_turisticos_1 == num_pontos_turisticos_2) {
                         printf ("EMPATE\n");
                         printf ("Atributo: Número de Pontos Turísticos");
                         printf ("Número de Pontos Turísticos de %s: %d\n", pais_1, num_pontos_turisticos_1);
@@ -240,7 +240,7 @@
                     printf ("Densidade Demográfica de %s: %d\n", pais_1, densidade_dem_1);
                     printf ("Densidade Demográfica de %s: %d\n", pais_2, densidade_dem_2);
                      }
-                    else if (densidade_dem_1 = densidade_dem_2) {
+                    else if (densidade_dem_1 == densidade_dem_2) {
                      printf ("EMPATE\n");
                      printf ("Atributo: Densidade Demográfica\n");
                      printf ("Densidade Demográfica de %s: %d\n", pais_1, densidade_dem_1);
